fefa0aa34b820530ba56f29571c0d3ac81ad04f8
younesz10 <yrz.ziewki18@gmail.com>
2024-05-20 21:06:07 -0400

    Adding images

--- script.js
+++ script.js
@@ -15,6 +15,71 @@ menuOptions.forEach(function (option) {
   });
 });
 
+// var slideIndex = 0;
+// var slides = document
+//   .getElementsByClassName("carousel")[0]
+//   .getElementsByTagName("img");
+// var prevBtn = document.getElementById("prevBtn");
+// var nextBtn = document.getElementById("nextBtn");
+
+// prevBtn.addEventListener("click", function () {
+//   slideIndex--;
+//   if (slideIndex < 0) {
+//     slideIndex = slides.length - 1;
+//   }
+//   showSlide();
+// });
+
+// nextBtn.addEventListener("click", function () {
+//   slideIndex++;
+//   if (slideIndex >= slides.length) {
+//     slideIndex = 0;
+//   }
+//   showSlide();
+// });
+
+// function showSlide() {
+//   for (var i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex].style.display = "block";
+// }
+
+// showSlide();
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+
+$(document).ready(function () {
+  $(".slickProduct").slick({
+    prevArrow: $(".custom-prev"),
+    nextArrow: $(".custom-next"),
+    slidesToShow: 3, // Adjust this value as needed
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
 var slideIndex = 0;
 var slides = document
   .getElementsByClassName("carousel")[0]
@@ -45,10 +110,15 @@ function showSlide() {
   slides[slideIndex].style.display = "block";
 }
 
-showSlide();
+// Auto-switch slides every 3 seconds
+function autoSwitch() {
+  setInterval(function () {
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    }
+    showSlide();
+  }, 3000); // 3 seconds
+}
 
-var loader = document.getElementById("preloader");
-
-window.addEventListener("load", function () {
-  loader.style.display = "none";
-});
+autoSwitch(); // Start auto-switching
